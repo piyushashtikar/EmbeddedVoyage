2cf17ce9e1d03f2eff001febd9683c865f489482
piyush ashtikar <piyushashtikar@bitmapper.com>
2017-01-19 13:27:22 +0530

    Ex1-10_complete

--- k&r/Ch1:The_Tutorial_Introduction/pg17_ex_1-7.c
+++ k&r/Ch1:The_Tutorial_Introduction/pg17_ex_1-7.c
@@ -4,5 +4,7 @@ main()
 {
 
 printf("\n\t%x\n",EOF);
+/*You can change the code for EOF with stty utitlity
+eg. stty eof CHAR */
 
 }
--- k&r/Ch1:The_Tutorial_Introduction/pg20_ex1-10.c
+++ k&r/Ch1:The_Tutorial_Introduction/pg20_ex1-10.c
@@ -11,10 +11,19 @@ if(c=='\t')
 printf("\\t");
 
 if(c== '\b')
-printf("<backspace>");
+{printf("\\");
+printf("b");}
+
+/*
+Terminal will eat backslash
+Nothing wrong here. If you were to run this program on a file that contained the backslash character, it would properly convert it. For terminal input, the program will not receive the backslash as it is managed by the input routines.
+
+If you still want to use it on terminal use 127 insted of '\b', then use command stty erase '' or stty erase \b , to change default behaviour of terminal. 
+*/
+
 
 if(c=='\\')
-printf("\\ \b\\");
+printf("\\\\");
 
 if(c!='\t'){
 	if(c!= '\b'){
